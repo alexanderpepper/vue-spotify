51ee645ceb8b771ff0847c55e2fc8eda049f7bc7
Mac Collins <macacollins@gmail.com>
2018-03-17 15:29:52 -0500

    Promisify getPlayer so that the first click works.

--- client/src/services/WebPlaybackService.js
+++ client/src/services/WebPlaybackService.js
@@ -24,42 +24,48 @@ const play = ({
 
 let singletonPlayer
 
-async function getPlayer () {
-  if (typeof singletonPlayer !== 'undefined') {
-    console.log('Returning already-initialized player.')
-    return singletonPlayer
-  }
+// TODO handle the case where this gets called twice in quick succession
+// if the function is called again before the first one resolves, it could create two players.
+function getPlayer () {
+  // This promise lets us pretend to return in the 'ready' event.
+  return new Promise(async resolve => {
+    if (typeof singletonPlayer !== 'undefined') {
+      console.log('Returning already-initialized player.')
+      resolve(singletonPlayer)
+      return
+    }
 
-  const token = await SpotifyService.getAccessToken()
+    const token = await SpotifyService.getAccessToken()
 
-  console.log('Token: ', token)
+    console.log('Token: ', token)
 
-  const player = new window.Spotify.Player({
-    name: 'Spotify .5 Web Player',
-    getOAuthToken: cb => { cb(token.url) }
+    const player = new window.Spotify.Player({
+      name: 'Spotify .5 Web Player',
+      getOAuthToken: cb => { cb(token.url) }
+    })
+
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message) })
+    player.addListener('authentication_error', ({ message }) => { console.error(message) })
+    player.addListener('account_error', ({ message }) => { console.error(message) })
+    player.addListener('playback_error', ({ message }) => { console.error(message) })
+
+    // Playback status updates
+    player.addListener('player_state_changed', state => { console.log(state) })
+
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id)
+
+      singletonPlayer = player
+
+      console.log('Returning newly-initialized player.')
+      resolve(player)
+    })
+
+    // Connect to the player!
+    player.connect()
   })
-
-  // Error handling
-  player.addListener('initialization_error', ({ message }) => { console.error(message) })
-  player.addListener('authentication_error', ({ message }) => { console.error(message) })
-  player.addListener('account_error', ({ message }) => { console.error(message) })
-  player.addListener('playback_error', ({ message }) => { console.error(message) })
-
-  // Playback status updates
-  player.addListener('player_state_changed', state => { console.log(state) })
-
-  // Ready
-  player.addListener('ready', ({ device_id }) => {
-    console.log('Ready with Device ID', device_id)
-  })
-
-  // Connect to the player!
-  player.connect()
-
-  singletonPlayer = player
-
-  console.log('Returning newly-initialized player.')
-  return player
 }
 
 class WebPlaybackService extends BaseService {

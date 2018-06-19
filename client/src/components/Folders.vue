bd39e46b7ee2036a367045178bb24aabf3b17d0f
Alex Pepper <alexander.pepper@gmail.com>
2018-06-19 12:48:50 -0500

    Remove VueDraggable lib

--- client/src/components/Folders.vue
+++ client/src/components/Folders.vue
@@ -1,18 +1,15 @@
 <template lang="pug">
   v-layout.folders.primary
     v-list.full-width
-      draggable(v-model='app.playlists')
-        v-list-tile(v-for='(playlist, index) in app.playlists', :key='index')
-          v-list-tile-content
-            v-list-tile-title {{ playlist.name }}
+      v-list-tile(v-for='(playlist, index) in app.playlists', :key='index')
+        v-list-tile-content
+          v-list-tile-title {{ playlist.name }}
 </template>
 
 <script>
-  import draggable from 'vuedraggable'
 
   export default {
     name: 'folders',
-    props: {app: Object},
-    components: {draggable}
+    props: {app: Object}
   }
 </script>

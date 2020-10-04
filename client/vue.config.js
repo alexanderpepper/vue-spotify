c63615a293ab66d3d5e661edb33dfa882f7984c1
Alex Pepper <alexander.pepper@gmail.com>
2020-10-04 08:20:17 -0500

    Fix hot-reloading on Safari

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  // transpileDependencies: [
-  //   'vuetify'
-  // ],
-  devServer: { headers: { 'Cache-Control': 'no-store' } },
-  chainWebpack: (config) => {
-    if (process.env.NODE_ENV === 'development') {
-      config.plugins.delete('preload')
-    }
-  }
+  devServer: { headers: { 'Cache-Control': 'no-store' } }
 }
